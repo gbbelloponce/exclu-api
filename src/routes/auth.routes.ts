d5c4413443a0bb9f4f8a70d29c927893115e5dac
gbbelloponce <personal.galo.bello@gmail.com>
2023-10-04 17:06:22 -0300

    Add auth routes in a group

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,46 @@
+import Elysia, { t } from 'elysia'
+
+import { AuthController } from '../controllers/auth.controller'
+
+export const auth = (app: Elysia) => {
+  // Group everything after 'auth/'
+  app.group('auth', (app) => {
+    // Identify Route
+    app.get('me/', AuthController.identify, {
+      body: t.Object({
+        username: t.String(),
+      }),
+      detail: {
+        tags: ['Auth'],
+      },
+    })
+
+    // Login Route
+    app.post('login/', AuthController.login, {
+      body: t.Object({
+        username: t.String(),
+        password: t.String(),
+      }),
+      detail: {
+        tags: ['Auth'],
+      },
+    })
+
+    // Register Route
+    app.post('register/', AuthController.register, {
+      body: t.Object({
+        name: t.String(),
+        username: t.String(),
+        email: t.String(),
+        password: t.String(),
+      }),
+      detail: {
+        tags: ['Auth'],
+      },
+    })
+
+    return app
+  })
+
+  return app
+}
