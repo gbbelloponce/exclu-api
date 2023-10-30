7cd0233f52b24528c1556ef10cec6f72a0565626
gbbelloponce <personal.galo.bello@gmail.com>
2023-10-30 20:57:38 -0300

    Workaround to get prisma client typed

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,5 +1,12 @@
+import type { PrismaClient as ImportedPrismaClient } from '@prisma/client'
 import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const { PrismaClient } = require('@prisma/client')
+
+const require = createRequire(import.meta.url ?? __filename)
+
+const { PrismaClient: RequiredPrismaClient } = require('@prisma/client')
+
+const _PrismaClient: typeof ImportedPrismaClient = RequiredPrismaClient
+
+export class PrismaClient extends _PrismaClient {}
 
 export const prisma = new PrismaClient()
